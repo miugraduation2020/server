2133257563e2dbd24a2892d8b033d2c27b347874
GannaAdnan <gannatullah1709438@miuegypt.edu.eg>
2021-04-24 09:37:21 +0200

    Update pathologistController.js

--- src/controllers/pathologistController.js
+++ src/controllers/pathologistController.js
@@ -30,28 +30,35 @@ exports.getPathologistsPatients = async (req, res) => {
 
 
 exports.assignPatients = async (req, res) => {
-
-    const newPatients = req.body.patientsToAssign;
+    const newPatients = []
     const pathologist = req.body.pathologistID;
-
+    await Pathologist.updateOne({ 'userId': pathologist }, { $push: { assignedPatients: newPatients } })
+    console.log("done1")
+    const pp = req.body.patientsToAssign;
+    if (pp.length > 0) {
+        newPatients.push(pp)
+        for (let index = 0; index < newPatients.length; index++) {
+            const element = newPatients[index];
+            await assigning(element);
+        }
+    } else {
+        for (let index = 0; index < newPatients.length; index++) {
+            const element = newPatients[index];
+            await assigning(element);
+        }
+    }
     console.log(newPatients);
     console.log(pathologist)
 
 
-    await Pathologist.updateOne({ 'userId': pathologist }, { $push: { assignedPatients: newPatients } })
-    console.log("done1")
-
-    for (let index = 0; index < newPatients.length; index++) {
-        const element = newPatients[index];
-        await assigning(element);
-    }
 
 
-    getPathologistsPatients;
+
+    this.getPathologistsPatients;
 }
 
 async function assigning(patEmail) {
-    await User.updateOne({ email: patEmail}, { $set: { isAssigned: true } })
+    await User.updateOne({ email: patEmail }, { $set: { isAssigned: true } })
     console.log('toggeled')
 }
 
