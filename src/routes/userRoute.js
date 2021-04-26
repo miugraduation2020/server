47256b1a9775188727cabdabb5f1473bb63dcd3d
Maysoon Elbedewi <64903798+maysoonelbedewi@users.noreply.github.com>
2021-04-26 05:44:45 +0200

    Update userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addUser, signIn, verifyEmail, forgotPassword, changePassword, getUsersData, getPatients, getPathologists } = require('../controllers/userController');
+const { addUser, signIn, verifyEmail, forgotPassword, changePassword, getUsersData, getPatients, getPathologists ,searchPatient} = require('../controllers/userController');
 const { destroySession } = require('../controllers/sessionController')
 const router = new express.Router()
 
@@ -17,5 +17,6 @@ router.post('/user/forgot-password', forgotPassword);
 router.post('/user/change-password', changePassword);
 router.get('/user/getUser', getUsersData);
 router.get('/user/logout', destroySession)
+router.post('/adminPatientsList', searchPatient);
 
 module.exports = router
