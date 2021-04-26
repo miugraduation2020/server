ab7d83125dc0451d3dccc893fbd15dcd61d90982
Maysoon Elbedewi <64903798+maysoonelbedewi@users.noreply.github.com>
2021-04-26 05:40:39 +0200

    Update userController.js
    Search in patients list

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -359,7 +359,46 @@ exports.getPatients = async (req, res) => {
 
 }
 
+/*Search in Patients List*/
 
+exports.searchPatient = async (req, res) => {
+    const searchby = req.body.searchby;
+    const search = req.body.searchPt;
+
+    if(searchby == 'Name'){
+        const pname = req.body.searchPt.split(' ');
+        
+    if(pname[1]){
+    const allPate= await User.find({ lastName: pname[1], firstName:pname[0]});
+    return res.render("adminPatientsList", { pateints: allPate, choice:searchby , search:search });}
+    else {
+    const allPate = await User.find().where('firstName').equals(pname[0]);
+
+    return res.render("adminPatientsList", { pateints: allPate, choice:searchby , search:search });}
+
+}
+
+
+    if(searchby == 'Email'){
+        const pemail = req.body.searchPt;
+    
+        const allPate = await User.find().where('email').equals(search);
+        
+        console.log(allPate[0]);
+    
+        return res.render("adminPatientsList", { pateints: allPate ,choice:searchby , search:search});}
+    
+        if(searchby == 'Phone Number'){
+            const pphone = req.body.searchPt;
+        
+            const allPate = await User.find().where('phoneNumber').equals(search);
+            
+            console.log(allPate[0]);
+        
+            return res.render("adminPatientsList", { pateints: allPate , choice:searchby , search:search});}
+        
+
+}
 
 
 
