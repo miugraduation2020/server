a7a907ee5aa2f1a76cb28b2fd82fd4011e2cc667
SaraNoorEldin <64905895+SaraNoorEldin@users.noreply.github.com>
2021-05-01 20:32:29 +0200

    Update FAQroutes.js

--- src/routes/FAQroutes.js
+++ src/routes/FAQroutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = new express.Router();
-const{addFAQ, getAllFAQ, deleteFAQ}=require("../controllers/FAQcontroller")
+const{addFAQ, deleteFAQ}=require("../controllers/FAQcontroller")
 
 
-router.get('/adminFAQ', getAllFAQ)
-router.post('/addfaq', addFAQ)
+//router.get('/adminFAQ', getAllFAQ)
+// router.post('/addfaq', addFAQ)
 router.post('/adminFAQ',deleteFAQ)
 
 module.exports = router
