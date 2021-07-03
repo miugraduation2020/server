d9b2fd0cbf0da9fae02127473fa33b742a3e2c2d
GannaAdnan <gannatullah1709438@miuegypt.edu.eg>
2021-07-03 21:49:56 +0200

    Index Updates

--- src/routes/FAQroutes.js
+++ src/routes/FAQroutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = new express.Router();
 const{addFAQ, deleteFAQ}=require("../controllers/FAQcontroller")
 
-
 //router.get('/adminFAQ', getAllFAQ)
 // router.post('/addfaq', addFAQ)
 router.post('/adminFAQ',deleteFAQ)
